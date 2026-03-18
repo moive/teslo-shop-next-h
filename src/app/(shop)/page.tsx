db05a156fb08034a52f03ef42638bc743e88c4f1
Moises Velasquez <moises007@gmail.com>
2026-03-17 23:56:42 -0500

    chore(directory): Directory structure and first pages

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,15 +2,15 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between py-8 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center  font-sans dark:bg-black">
+      <div className="flex min-h-screen w-full flex-col items-center justify-between py-8 px-16  sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="text-3xl font-semibold">Hello world</h1>
           <h1 className={`text-3xl font-semibold ${titleFont.className}`}>
             Hello world
           </h1>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center  font-sans dark:bg-black">
+      <div className="flex min-h-screen w-full flex-col items-center justify-between py-8 px-16  sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className={`text-3xl font-semibold ${titleFont.className}`}>
+            Login page
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}

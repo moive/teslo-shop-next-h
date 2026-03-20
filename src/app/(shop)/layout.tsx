49aa094fec7b11bb876d185d3a98879cb87a23fb
Moises Velasquez <moises007@gmail.com>
2026-03-19 23:34:11 -0500

    feat(top-nav): TopNavigation

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,3 +1,10 @@
+import TopMenu from "@/components/ui/top-menu/TopMenu";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <main className="min-h-screen bg-red-500">{children}</main>;
+  return (
+    <main className="min-h-screen">
+      <TopMenu />
+      {children}
+    </main>
+  );
 }
---
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -0,0 +1,57 @@
+import { titleFont } from "@/config/fonts";
+import Link from "next/link";
+import React from "react";
+import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
+
+const TopMenu = () => {
+  return (
+    <nav className="flex px-5 justify-between items-center w-full">
+      <div>
+        <Link href="/">
+          <span className={`${titleFont.className} antialiased font-bold`}>
+            Teslo
+          </span>
+          <span> | Shop</span>
+        </Link>
+      </div>
+      <div className="hidden sm:block">
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/men"
+        >
+          Men
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/women"
+        >
+          Women
+        </Link>
+        <Link
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          href="/category/kids"
+        >
+          Kids
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <Link href="/search" className="mx-2">
+          <IoSearchOutline className="w-5 h-5" />
+        </Link>
+        <Link href="/cart" className="mx-2">
+          <div className="relative">
+            <span className="absolute text-xs rounded-full px-1 font-bold bg-blue-700 text-white -top-2 -right-2">
+              3
+            </span>
+            <IoCartOutline className="w-5 h-5" />
+          </div>
+        </Link>
+        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+          Menu
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default TopMenu;

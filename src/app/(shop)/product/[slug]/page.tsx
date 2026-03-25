5dcd87c48016e114088c47f556bbc2ba6fa573fc
Moises Velasquez <moises007@gmail.com>
2026-03-25 00:30:30 -0500

    feat(product): add screen product

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,37 @@
-export default function ProductPage() {
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default async function ProductPage({ params }: Props) {
+  const { slug } = await params;
+  const product = initialData.products.find((p) => p.slug === slug);
+
+  if (!product) notFound();
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2">Hello World {slug}</div>
+      {/* Details */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+        {/* Selectors Size */}
+        {/* Selectors quantity */}
+        {/* button */}
+        <button className="btn-primary my-5">Add to cart</button>
+        {/* Description */}
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }

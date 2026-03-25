2a9516fd61c3bd0d13c87ab55326d8e463786bb0
Moises Velasquez <moises007@gmail.com>
2026-03-25 12:26:00 -0500

    feat(slideshow): add slideshow desktop and mobile

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,9 @@
-import { QuantitySelector, SizeSelector } from "@/components";
+import {
+  ProductMobileSlideshow,
+  ProductSlideshow,
+  QuantitySelector,
+  SizeSelector,
+} from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -18,7 +23,20 @@ export default async function ProductPage({ params }: Props) {
   return (
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow */}
-      <div className="col-span-1 md:col-span-2">Hello World {slug}</div>
+      <div className="col-span-1 md:col-span-2">
+        {/* Slideshow mobile */}
+        <ProductMobileSlideshow
+          images={product.images}
+          title={product.title}
+          className="block md:hidden"
+        />
+        {/* Slideshow desktop */}
+        <ProductSlideshow
+          images={product.images}
+          title={product.title}
+          className="hidden md:block"
+        />
+      </div>
       {/* Details */}
       <div className="col-span-1 px-5">
         <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>

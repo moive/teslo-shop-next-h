1d98f3f26c14e83937d4348d4d77632e3a11f07a
Moises Velasquez <moises007@gmail.com>
2026-03-26 00:24:22 -0500

    Verify order page

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,95 @@
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+const productInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function CheckoutPage() {
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000]">
+        <Title title="Verify your order" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Cart */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Ajust elements</span>
+            <Link href="/cart">Edit cart </Link>
+
+            {/* Items */}
+            {productInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>{product.price} x 3</p>
+                  <p className="font-bold">Subtotal: ${product.price * 3}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Checkout */}
+          <div className="rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2 font-bold">Delivery address</h2>
+            <div className="mb-10">
+              <p className="text-xl">Fernando Herrera</p>
+              <p>Av. Siempre viva 123</p>
+              <p>Col. Centro</p>
+              <p>Alcaldía Cuauhtémoc</p>
+              <p>Ciudad de México</p>
+              <p>CP 12345</p>
+              <p>123.123.123</p>
+            </div>
+
+            <div className="w-full h-0.5 rounded bg-gray-300 mb-10" />
+
+            <h2 className="text-2xl mb-2 font-bold">Order summary</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Products</span>
+              <span className="text-right">3 articles</span>
+
+              <span>Sub-total</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Sales tax (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-2xl text-right">$ 100</span>
+            </div>
+            <div className="mt-5 mb-2 w-full">
+              <p className="mb-5">
+                <span className="text-xs">
+                  By clicking on {"Place order"}, you agree to our{" "}
+                  <a href="#" className="underline">
+                    Terms and Conditions
+                  </a>{" "}
+                  and{" "}
+                  <a href="#" className="underline">
+                    Privacity Policy
+                  </a>
+                </span>
+              </p>
+              <Link
+                href="/orders/1234"
+                className="flex btn-primary justify-center"
+              >
+                Place order
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

a8dc112be39c5015cb8742d130136e3a6b7d571d
Moises Velasquez <moises007@gmail.com>
2026-04-07 11:05:51 -0500

    feat(store): implement addProductToCart

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,10 +1,33 @@
-import { CartProduct } from "@/interfaces";
 import { create } from "zustand";
+import { CartProduct } from "@/interfaces";
 
 interface State {
   cart: CartProduct[];
+
+  addProductToCart: (product: CartProduct) => void;
 }
 
-export const useCartStore = create<State>((set) => ({
+export const useCartStore = create<State>((set, get) => ({
   cart: [],
+  addProductToCart: (product: CartProduct) => {
+    const { cart } = get();
+
+    const productInCart = cart.some(
+      (item) => item.id === product.id && item.size === product.size,
+    );
+
+    if (!productInCart) {
+      set({ cart: [...cart, product] });
+      return;
+    }
+
+    const udpatedCartProducts = cart.map((item) => {
+      if (item.id === product.id && item.size === product.size) {
+        return { ...item, quantity: item.quantity + product.quantity };
+      }
+      return item;
+    });
+
+    set({ cart: udpatedCartProducts });
+  },
 }));

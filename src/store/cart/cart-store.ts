464c564aa41eb49315ec5f5f2ef59cd5b248424d
Moises Velasquez <moises007@gmail.com>
2026-04-07 10:39:53 -0500

    feat(store): add Zustand cart store with initial state

---
+++ src/store/cart/cart-store.ts
@@ -0,0 +1,10 @@
+import { CartProduct } from "@/interfaces";
+import { create } from "zustand";
+
+interface State {
+  cart: CartProduct[];
+}
+
+export const useCartStore = create<State>((set) => ({
+  cart: [],
+}));

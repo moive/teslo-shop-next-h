c95a207f48f7fa4704ff04860b21d2ce383b70c3
Moises Velasquez <moises007@gmail.com>
2026-04-20 10:05:21 -0500

    feat(orderSummary): add component orderSummary

---
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useCartStore } from "@/store";
+import { useEffect, useState } from "react";
+import { useShallow } from "zustand/shallow";
+
+export const OrderSummary = () => {
+  const [loaded, setLoaded] = useState(false);
+  const { subTotal, tax, total, itemsInCart } = useCartStore(
+    useShallow((state) => state.getSummaryInformation()),
+  );
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) return <p>Loading...</p>;
+
+  return (
+    <div className="grid grid-cols-2">
+      <span>No. Products</span>
+      <span className="text-right">
+        {itemsInCart === 1 ? "1 item" : `${itemsInCart} items`}
+      </span>
+
+      <span>Sub-total</span>
+      <span className="text-right">$ {subTotal}</span>
+
+      <span>Sales tax (15%)</span>
+      <span className="text-right">$ {tax}</span>
+
+      <span className="mt-5 text-2xl">Total:</span>
+      <span className="mt-5 text-2xl text-right">$ {total}</span>
+    </div>
+  );
+};

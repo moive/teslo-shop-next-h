f6e5c84d55944da5e42939e8d6b6fb54ad366336
Moises Velasquez <moises007@gmail.com>
2026-08-10 19:37:03 -0500

    fix: fix loader footer

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { titleFont } from "@/config/fonts";
 import Link from "next/link";
 
 export const Footer = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
   return (
     <div className="flex w-full justify-center text-xs mb-10">
       <Link href="/">
-        <span className={`${titleFont.className} antialiased font-bold`}>
+        <span
+          className={`${loaded ? titleFont.className : ""} antialiased font-bold`}
+        >
           Teslo
         </span>
         <span> | shop</span>
